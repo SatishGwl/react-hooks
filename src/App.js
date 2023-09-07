cc0780b078827469dee60f513c0320e1086436a9
Satish Sharma <Satish.Sharma@lntinfotech.com>
2023-09-07 15:42:51 +0530

    useRef hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
-// import Counter from "./components/Counter";
-import UseStateHook from "./UseStateHook/UseStateHook";
-class Car extends React.Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       count: 0,
-  //     };
-  //   }
-  componentDidMount() {
-    console.log("ConmponentDidMount: when Compnent rendered first time");
+// import React, { useRef } from "react";
+// import "./App.css";
+// // import Counter from "./components/Counter";
+// // import UseStateHook from "./UseStateHook/UseStateHook";
+// import Counter from "./components/UseReducerHook/Counter";
+
+import React, { useRef, useState } from "react";
+
+const App = () => {
+  const refElement = useRef();
+  const [name, setName] = useState("satish");
+
+  console.log(refElement);
+  function Reset() {
+    setName("");
+    refElement.current.focus();
   }
-  increment() {
-    this.setState({ count: this.state.count + 1 });
+  function handleInput() {
+    refElement.current.style.color = "red";
   }
-  render() {
-    console.log("Update");
-    return (
-      <>
-        <UseStateHook />
-      </>
-    );
-  }
-}
-export default Car;
+  return (
+    <div>
+      <h1>Learning useRef</h1>
+      <input
+        type="text"
+        ref={refElement}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      ></input>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={handleInput}>Hanlde Input</button>
+    </div>
+  );
+};
+
+export default App;
+
+// class Car extends React.Component {
+
+//   render() {
+//     console.log("Update");
+//     return (
+//       <div className="App">
+//         <h1>Learning useRef</h1>
+//       </div>
+//     );
+//   }
+// }
+// export default Car;
